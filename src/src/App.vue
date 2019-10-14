c3991c511f2569885a39d5916eb4b93d5379d34b
ymsk.sky <ymsk.sky.95@gmail.com>
2019-10-14 13:27:00 +0900

    データバインディング・コンポーネント化を体験

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{ msg }}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import myheader from './components/myheader'
+
+export default {
+  components: {
+    myheader
+  },
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
     }
   }
 }
-</style>
+</script>
